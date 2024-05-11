01571bc5129cb0df1a61921c9c3ba80469a8e55e
liam <yang.qq123@163.com>
2024-05-11 17:57:59 +0800

    编写bot

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-## UI设计师平台相关服务
+# UI设计师平台相关服务
+
+# pip清华镜像源
+-i https://pypi.tuna.tsinghua.edu.cn/simple some-package
---
+++ midijourney-bot/main.py
@@ -0,0 +1,10 @@
+from discord import Intents, Message
+from discord.ext import commands
+
+intents =  Intents.default()
+intents.message_content = True
+discord_bot = commands.Bot(command_prefix="", intents=intents)
+
+@discord_bot.event
+async def on_read():
+    pass
